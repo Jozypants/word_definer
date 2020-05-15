8b8ae9ee435a4dfe4c078fffa61e678812352868
Jozy Kinnaman <jozypants@gmail.com>
2020-05-15 09:46:42 -0700

    add class Definition and intiialize method.

---
+++ lib/definition.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+class Definition
+  attr_reader :id
+  attr_accessor :definition :word_id
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize (definition, id, word_id)
+    @definition = definition
+    @word_id = word_id
+    @id = id || @total_rows += 1
+  end
+
+  def ==(definition_to_compare)
+    (self.definition() == definition_to_compare.definition()) && (self.word_id() == definition_to_compare.word_id())
+  end
+  
+  def self.all
+    @@definitions.values
+  end
+  
+  def save
+    @@definitions[self.id] = Definition.new(self.definition, self.word_id, self.id)
+  end
+
+  # def self.find(id)
+  #   @@definition[id]
+  # end
+
+  # def update(definition, word_id)
+  #   self.definition = definition
+  #   self.word_id = word_id
+  #   @@definitions[self.id] = Definition.new(self.definition, self.word_id, self.id)
+  # end
+
+  # def delete
+  #   @@definitions.delete(self.id)
+  # end
+  
+  # def self.clear
+  #   @@definitions = {}
+  # end
+
+  # def self.find_by_word(word_id)
+  #   definitions = []
+  #   @@definitions.values.each do |definition|
+  #     if definition.word_id == word_id
+  #       definitions.push(definition)
+  #     end
+  #   end
+  #   definitions
+  # end
+
+  def word
+    Word.find(self.word_id)
+  end
+
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-# require 'definition'
+require 'definition'
 require 'word'
 require 'pry'
 
