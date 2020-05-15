d356d177d56626e039526c5dd5f4fe9d6a86bf6b
Jozy Kinnaman <jozypants@gmail.com>
2020-05-15 10:13:20 -0700

    write test for .all. Expect to FAIL.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -13,9 +13,20 @@ describe '#Definition' do
   
   describe('#==') do
     it ("is the same def if it has the same attributes as another def")do
-    definition = Definition.new("delay or postpone action; put off doing something.", @word.id, nil)
+    definition1 = Definition.new("delay or postpone action; put off doing something.", @word.id, nil)
     definition2 = Definition.new("delay or postpone action; put off doing something.", @word.id, nil)
-    expect(definition2).to(eq(definition2))
+    expect(definition1).to(eq(definition2))
     end
   end
+
+  describe('.all') do
+    it("returns a list of all definitions") do
+    definition1 = Definition.new("delay or postpone action; put off doing something.", @word.id, nil)
+    definition1.save()
+    definition2 = Definition.new("Put somethingoff", @word.id, nil)
+    definition2.save()
+    expect(Definition.all).to(eq([definition1, definition2]))
+    end
+  end
+
 end
