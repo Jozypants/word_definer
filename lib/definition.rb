9a38a4f6dcccd3e5bef8343abaa870891c4a1b9b
Jozy Kinnaman <jozypants@gmail.com>
2020-05-15 11:19:39 -0700

    rewrite variables for clarity. Rerun test for #==. Expect to pass.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,61 +1,43 @@
-require 'pry'
-
 class Definition
   attr_reader :id
-  attr_accessor :definition, :word_id
+  attr_accessor :meaning, :word_id
 
   @@definitions = {}
   @@total_rows = 0
 
-  def initialize (definition, id, word_id)
-    @definition = definition
+  def initialize(meaning, word_id, id)
+    @meaning = meaning
     @word_id = word_id
     @id = id || @@total_rows += 1
   end
 
-  def ==(definition_to_compare)
-    if (self.word_id() == definition_to_compare.word_id())
-    end
+  def ==(word_to_compare)
+    (self.meaning() == word_to_compare.meaning()) && (self.word_id() == word_to_compare.word_id())
   end
-  
+
   def self.all
-    @@definitions.values()
+    @@definitions.values
   end
-  
+
   def save
-    @@definitions[self.id] = Definition.new(self.definition, self.word_id, self.id)
+    @@definitions[self.id] = Definition.new(self.meaning, self.word_id, self.id)
   end
 
   def self.find(id)
     @@definitions[id]
   end
 
-  def update(definition, word_id)
-    self.definition = definition
-    self.word_id = word_id
-    @@definitions[self.id] = Definition.new(self.definition, self.word_id, self.id)
+  def update(meaning, word_id)
+    self.meaning = meaning
+    self.album_id = album_id
+    @@definitions[self.id] = Definition.new(self.meaning, self.album_id, self.id)
   end
 
   def delete
     @@definitions.delete(self.id)
   end
-  
+
   def self.clear
     @@definitions = {}
   end
-
-  def self.find_by_word(word_id)
-    definitions = []
-    @@definitions.values.each do |definition|
-      if definition.word_id == word_id
-        definitions.push(definition)
-      end
-    end
-    definitions
-  end
-
-  def word
-    Word.find(self.word_id)
-  end
-
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,52 +1,81 @@
 require 'rspec'
-require 'definition'
+require 'definiton'
 require 'word'
 require 'pry'
 
-describe '#Definition' do
+describe '#Definitions' do
 
-  before (:each) do
+  before(:each) do
     Word.clear()
     Definition.clear()
-    @word = Word.new("procrastinate", nil)
-    @word.save
+    @word = Word.new("Procrastinate", nil)
+    @word.save()
   end
-  
+
   describe('#==') do
-    it ("is the same def if it has the same attributes as another def") do
-    definition1 = Definition.new("delay or postpone action", @word.id, nil)
-    definition2 = Definition.new("delay or postpone action", @word.id, nil)
-    expect(definition1).to(eq(definition2))
+    it("is the same song if it has the same attributes as another song") do
+      definition1 = Definition.new("to put something off", @word.id, nil)
+      definition2 = Definition.new("to put something off", @word.id, nil)
+      expect(definition1).to(eq(definition2))
     end
   end
 
-  describe('.all') do
-    it("returns a list of all definitions") do
-      definition1 = Definition.new("delay or postpone action", @word.id, nil)
-      definition1.save()
-      definition2 = Definition.new("Put something off", @word.id, nil)
-      definition2.save()
-      expect(Definition.all).to(eq([definition1, definition2]))
-    end
-  end
+  # describe('.all') do
+  #   it("returns a list of all songs") do
+  #     song = Song.new("Giant Steps", @album.id, nil)
+  #     song.save()
+  #     song2 = Song.new("Naima", @album.id, nil)
+  #     song2.save()
+  #     expect(Song.all).to(eq([song, song2]))
+  #   end
+  # end
 
-   describe('.clear') do
-    it("clears all definitions") do
-      definition1 = Definition.new("delay or postpone action", @word.id, nil)
-      definition1.save()
-      definition2 = Definition.new("Put something off", @word.id, nil)
-      definition2.save()
-      Definition.clear()
-      expect(Definition.all).to(eq([]))
-    end
-  end
+  # describe('.clear') do
+  #   it("clears all songs") do
+  #     song = Song.new("Giant Steps", @album.id, nil)
+  #     song.save()
+  #     song2 = Song.new("Naima", @album.id, nil)
+  #     song2.save()
+  #     Song.clear()
+  #     expect(Song.all).to(eq([]))
+  #   end
+  # end
 
-   describe('#word') do
-    it("finds the word a definition belongs to") do
-      definition1 = Definition.new("delay or postpone action", @word.id, nil)
-      definition1.save()
-      expect(definition1.word()).to(eq(@word))
-    end
-  end
+  # describe('#save') do
+  #   it("saves a song") do
+  #     song = Song.new("Naima", @album.id, nil)
+  #     song.save()
+  #     expect(Song.all).to(eq([song]))
+  #   end
+  # end
 
+  # describe('.find') do
+  #   it("finds a song by id") do
+  #     song = Song.new("Giant Steps", @album.id, nil)
+  #     song.save()
+  #     song2 = Song.new("Naima", @album.id, nil)
+  #     song2.save()
+  #     expect(Song.find(song.id)).to(eq(song))
+  #   end
+  # end
+
+  # describe('#update') do
+  #   it("updates an song by id") do
+  #     song = Song.new("Naima", @album.id, nil)
+  #     song.save()
+  #     song.update("Mr. P.C.", @album.id)
+  #     expect(song.name).to(eq("Mr. P.C."))
+  #   end
+  # end
+
+  # describe('#delete') do
+  #   it("deletes an song by id") do
+  #     song = Song.new("Giant Steps", @album.id, nil)
+  #     song.save()
+  #     song2 = Song.new("Naima", @album.id, nil)
+  #     song2.save()
+  #     song.delete()
+  #     expect(Song.all).to(eq([song2]))
+  #   end
+  # end
 end
