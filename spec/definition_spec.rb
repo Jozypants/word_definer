b81f08bb18af0fe7f7dbd98631b65db4f9eb4f0b
Jozy Kinnaman <jozypants@gmail.com>
2020-05-15 10:02:55 -0700

    add test to overwrite == operator. Expect to fail.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -11,4 +11,11 @@ describe '#Definition' do
     @word.save
   end
   
+  describe('#==') do
+    it ("is the same def if it has the same attributes as another def")do
+    definition = Definition.new("delay or postpone action; put off doing something.", @word.id, nil)
+    definition2 = Definition.new("delay or postpone action; put off doing something.", @word.id, nil)
+    expect(definition2).to(eq(definition2))
+    end
+  end
 end
