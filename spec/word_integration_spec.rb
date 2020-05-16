296c87ec3b7dbaaf294097918ff9a6a88cd5e497
Jozy Kinnaman <jozypants@gmail.com>
2020-05-16 11:21:43 -0700

    add test for definition path. Expect fail (because I'm not sure if I wrote it correctly.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -11,3 +11,12 @@ describe('create an word path', {:type => :feature}) do
     expect(page).to have_content('procrastinate')
   end
 end
+
+describe('create an definition path', {:type => :feature}) do
+  it('creates definition and then adds it to the list') do
+    visit('/homepage/:id/definitions')
+    fill_in('word_def', :with => 'to delay')
+    click_on('go!')
+    expect(page).to have_content('to delay')
+  end
+end
