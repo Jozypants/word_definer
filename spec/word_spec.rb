6dc13c672c1c4c4ba271961cf079a6d891e96cdf
Jozy Kinnaman <jozypants@gmail.com>
2020-05-20 20:52:07 -0700

    add test for Word.clear. Expect to pass.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,67 +10,78 @@ describe '#Word' do
   end
 
 
-  describe('#save') do
-    it("saves a word") do
-      word = Word.new("tenacious", nil)
-      word.save()
-      word2 = Word.new("thorough", nil)
-      word2.save()
-      expect(Word.all).to(eq([word, word2]))
+    describe('#save') do
+      it("saves a word") do
+        word = Word.new("tenacious", nil)
+        word.save()
+        word2 = Word.new("thorough", nil)
+        word2.save()
+        expect(Word.all).to(eq([word, word2]))
+      end
     end
-  end
 
-  describe('#==') do
-    it("is the same word if it has the same attributes as another word") do
-      word = Word.new("lightning", nil)
-      word2 = Word.new("lightning", nil)
-      expect(word).to(eq(word2))
+    describe('#==') do
+      it("is the same word if it has the same attributes as another word") do
+        word = Word.new("lightning", nil)
+        word2 = Word.new("lightning", nil)
+        expect(word).to(eq(word2))
+      end
     end
-  end
 
-  describe('.find') do
-    it("finds a word by id") do
-      word = Word.new("tenacious", nil)
-      word.save()
-      word2 = Word.new("thorough", nil)
-      word2.save() 
-      expect(Word.find(word.id)).to(eq(word))
+    describe('.find') do
+      it("finds a word by id") do
+        word = Word.new("tenacious", nil)
+        word.save()
+        word2 = Word.new("thorough", nil)
+        word2.save() 
+        expect(Word.find(word.id)).to(eq(word))
+      end
     end
-  end
-  
-  describe('#definitions') do
-    it("returns a word's definitions") do
-      word = Word.new("Procrastinate", nil)
-      word.save()
-      definition1 = Definition.new("delay or postpone action", word.id, nil)
-      definition2 = Definition.new("to put something off", word.id, nil)
-      definition2.save()
-      expect(word.definitions).to(eq([definition1, definition2]))
+    
+    describe('#definitions') do
+      it("returns a word's definitions") do
+        word = Word.new("Procrastinate", nil)
+        word.save()
+        definition1 = Definition.new("delay or postpone action", word.id, nil)
+        definition2 = Definition.new("to put something off", word.id, nil)
+        definition2.save()
+        expect(word.definitions).to(eq([definition1, definition2]))
+      end
     end
-  end
 
     describe('#update') do
-    it("updates a word by id") do
-      word1 = Word.new("propose", nil)
-      word1.save()
-      word1.update("purpose")
-      expect(word1.name).to(eq("purpose"))
+      it("updates a word by id") do
+        word1 = Word.new("propose", nil)
+        word1.save()
+        word1.update("purpose")
+        expect(word1.name).to(eq("purpose"))
+      end
     end
-  end
 
-  describe('#delete') do
-    it("deletes an word by id") do
+    describe('#delete') do
+      it("deletes an word by id") do
+        word1 = Word.new("purpose", nil)
+        word1.save()
+        word2 = Word.new("to put something off", nil)
+        word2.save()
+        word1.delete()
+        expect(Word.all).to(eq([word2]))
+      end
+    end
+
+    describe('.all') do
+      it("is returns an empty array when there are no words") do
+        expect(Word.all).to(eq([]))
+      end
+    end
+
+    describe('.clear')do
+    it("clears all words") do
       word1 = Word.new("purpose", nil)
       word1.save()
       word2 = Word.new("to put something off", nil)
       word2.save()
-      word1.delete()
-      expect(Word.all).to(eq([word2]))
-    end
-  end
-
-  describe(.all) do
-    it("is returns an empty array when there are no words") do
+      Word.clear()
       expect(Word.all).to(eq([]))
     end
   end
